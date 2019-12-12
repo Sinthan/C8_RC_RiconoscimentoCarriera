4ab5c8733f6a46d20a9e9ef24d156942469b539e
GerardoDam <57265408+GerardoDam@users.noreply.github.com>
2019-12-12 16:24:06 +0100

    AddDB

--- SQL_CREATE_TABLE.sql
+++ SQL_CREATE_TABLE.sql
@@ -2,61 +2,145 @@ DROP DATABASE IF EXISTS englishvalidation;
 CREATE DATABASE englishvalidation COLLATE 'utf8_general_ci';
 USE englishvalidation;
 
-CREATE TABLE USER (
-EMAIL varchar(50) not null,
-NAME varchar(50) not null,
-SURNAME varchar(50) not null,
-SEX char not null,
-PASSWORD varchar(50) not null,
-USER_TYPE tinyint(1) not null,
-primary key (EMAIL)
+CREATE TABLE USER(
+	EMAIL varchar(50) not null,
+	NAME varchar(50) not null,
+	SURNAME varchar(50) not null,
+	SEX char not null,
+	PASSWORD varchar(50) not null,
+	USER_TYPE tinyint(1) not null,
+	DATE_REGISTRATION date null,  /*Attributo inserito per la gestione degli account temporanei*/
+	primary key (EMAIL)
 );
+
+/*TABELLE AGGIUNTE PER RC _____________________________________________________________________________________________________*/
+CREATE TABLE UC( 
+	EMAIL varchar(50) not null,
+	PASSWORD varchar(50) not null,    /* <---- CONTROLLARE TABELLA ERRORE INT*/
+    TELEPHONE VARCHAR(10) not null,   /* <---- MODIFICATO IN VARCHAR*/
+    FAX VARCHAR(10) default null,     /* <---- MODIFICATO IN VARCHAR*/
+    primary key (EMAIL)
+);
+
+CREATE TABLE REPORT( 
+	ID_REPORT int(20) not null AUTO_INCREMENT,
+    NOTE varchar(5000) default null,
+    primary key (ID_REPORT)
+);
+alter table REPORT AUTO_INCREMENT=1;
+
+CREATE TABLE UNIVERSITY(
+	NAME varchar(100) not null,
+    primary key (NAME)
+);
+
+CREATE TABLE REQUEST_RC(
+	ID_REQUEST int(20) not null AUTO_INCREMENT,
+    DATE_REQUEST date not null,
+    STATE int(20) not null,
+    FK_UNIVERSITY varchar(50) not null,
+    FK_USER varchar(50) not null,
+    FK_REPORT int(20),     
+    FK_EMAIL_UC varchar(50) not null,
+    foreign key (FK_UNIVERSITY) references UNIVERSITY(NAME),
+    foreign key (FK_USER) references USER(EMAIL),
+    foreign key (FK_REPORT) references REPORT(ID_REPORT),
+    foreign key (FK_EMAIL_UC) references UC(EMAIL),
+    primary key (ID_REQUEST)
+);
+alter table REQUEST_RC AUTO_INCREMENT=1;
+
+CREATE TABLE VALIDATE_EXAM(
+	ID_EXAM_VALIDATE int(20) not null AUTO_INCREMENT,
+    NAME_EXAM varchar(50) not null,
+    CFU_CONVALIDATED tinyint(1) not null,
+    MODE_VALIDATION varchar(50) not null,
+    FK_ID_REPORT int(20) not null,
+    foreign key (FK_ID_REPORT) references REPORT(ID_REPORT),
+	primary key (ID_EXAM_VALIDATE)
+);
+alter table VALIDATE_EXAM AUTO_INCREMENT=1;
+
+CREATE TABLE FILE_PDF(
+	ID_PDF int(20) not null AUTO_INCREMENT,
+    LINK_PDF  varchar(100) not null,
+    FK_ID_REQUEST_RC int(20) not null,
+    foreign key (FK_ID_REQUEST_RC) references REQUEST_RC(ID_REQUEST),
+    primary key(ID_PDF)
+);
+alter table FILE_PDF AUTO_INCREMENT=1;
+
+CREATE TABLE EXAM(
+	ID_EXAM int(20) AUTO_INCREMENT,
+    NAME varchar(50) not null,
+    CFU tinyint(1) not null,
+    LINK_PROGRAM varchar(256) not null,
+    primary key(ID_EXAM)
+);
+alter table EXAM AUTO_INCREMENT=1;
+
+CREATE TABLE CONTAINS(
+	FK_REQUEST_RC int(20) not null,
+    FK_EXAM int(20) not null,
+    foreign key (FK_REQUEST_RC) references REQUEST_RC(ID_REQUEST),
+    foreign key (FK_EXAM) references EXAM(ID_EXAM),
+    primary key (FK_REQUEST_RC, FK_EXAM)
+);
+
+CREATE TABLE SUGESTION(
+NAME_UNIVERSITY varchar(50) not null,
+NAME_EXAM_EXTERN varchar(50) not null,
+NUMBER_CFU_EXTERN varchar(30) not null,
+MODE_VALIDATION varchar(50) not null,
+DATE_VALIDATION date not null
+
+);
+/*FINE TABELLE UTILIZZATE IN RC _____________________________________________________________________________________________________*/
+
 
 CREATE TABLE SYSTEM_ATTRIBUTE (
-SLUG varchar(50) not null, 
-VALUE varchar(100) not null, 
-FK_USER varchar(50) not null,
-primary key (SLUG),
-foreign key (FK_USER) references USER(EMAIL)
+	SLUG varchar(50) not null, 
+	VALUE varchar(100) not null, 
+	FK_USER varchar(50) not null,
+	primary key (SLUG),
+	foreign key (FK_USER) references USER(EMAIL)
 );
 
-
 CREATE TABLE REQUEST (
-ID_REQUEST int(20) not null AUTO_INCREMENT,
-CERTIFICATE_SERIAL VARCHAR(50) not null,
-LEVEL varchar(7) not null,
-RELEASE_DATE date not null, 
-EXPIRY_DATE date not null, 
-YEAR year not null, 
-REQUESTED_CFU tinyint(2) not null, 
-SERIAL int(10) not null, 
-VALIDATED_CFU tinyint(2) not null, 
-FK_USER varchar(50) not null,
-FK_CERTIFIER int(20) not null, 
-FK_STATE int(20) not null, 
-primary key(ID_REQUEST)
+	ID_REQUEST int(20) not null AUTO_INCREMENT,
+	CERTIFICATE_SERIAL VARCHAR(50) not null,
+	LEVEL varchar(7) not null,
+	RELEASE_DATE date not null, 
+	EXPIRY_DATE date not null, 
+	YEAR year not null, 
+	REQUESTED_CFU tinyint(2) not null, 
+	SERIAL int(10) not null, 
+	VALIDATED_CFU tinyint(2) not null, 
+	FK_USER varchar(50) not null,
+	FK_CERTIFIER int(20) not null, 
+	FK_STATE int(20) not null, 
+	primary key(ID_REQUEST)
 );
 
 CREATE TABLE ATTACHED (
-ID_ATTACHED int(20) not null AUTO_INCREMENT,
-FILENAME varchar(50) not null,
-FK_REQUEST int(20) not null,
-FK_USER varchar(50) not null,
-primary key(ID_ATTACHED)
+	ID_ATTACHED int(20) not null AUTO_INCREMENT,
+	FILENAME varchar(50) not null,
+	FK_REQUEST int(20) not null,
+	FK_USER varchar(50) not null,
+	primary key(ID_ATTACHED)
 );
 
-
 CREATE TABLE ENTE (
-ID_ENTE int(20) not null AUTO_INCREMENT,
-EMAIL varchar(50) not null,
-NAME varchar(100) not null,
-SITE varchar(50) not null,
-STATO TINYINT not null DEFAULT 0,
-primary key (ID_ENTE)
+	ID_ENTE int(20) not null AUTO_INCREMENT,
+	EMAIL varchar(50) not null,
+	NAME varchar(100) not null,
+	SITE varchar(50) not null,
+	STATO TINYINT not null default 0,
+	primary key (ID_ENTE)
 );
 
 CREATE TABLE STATE (
-ID_STATE int(20) not null AUTO_INCREMENT, 
-DESCRIPTION varchar(100) not null,
-primary key (ID_STATE)
+	ID_STATE int(20) not null AUTO_INCREMENT, 
+	DESCRIPTION varchar(100) not null,
+	primary key (ID_STATE)
 );
--- SQL_INSERT_INTO.sql
+++ SQL_INSERT_INTO.sql
@@ -1,9 +1,57 @@
 USE englishvalidation;
-INSERT INTO user
-VALUES('segreteria@unisa.it','Segreteria','Studenti','M','4bb47fd2a6c598d2a52ef7de3473fd3ea8401a9b','1'); 
 
-INSERT INTO user
-VALUES('fferrucci@unisa.it','Filomena','Ferrucci','M','4bb47fd2a6c598d2a52ef7de3473fd3ea8401a9b','2');
+
+INSERT INTO REQUEST VALUES (1,'B.6.56546','A1','2017-05-25','2018-05-25',2018,3,512103579,6,'prova@unisa.it',2,2);
+INSERT INTO REQUEST VALUES (2,'B.6.56546','A1','2017-05-25','2018-05-25',2018,3,512103579,6,'prova2@unisa.it',2,3);
+INSERT INTO REQUEST VALUES (3,'B.6.56546','A1','2017-05-25','2018-05-25',2018,3,512103579,6,'prova3@unisa.it',3,1);
+INSERT INTO REQUEST VALUES (4,'B.6.56546','A1','2017-05-25','2018-05-25',2018,3,512103579,6,'prova4@unisa.it',4,2);
+INSERT INTO REQUEST VALUES (5,'B.6.56546','A1','2017-05-25','2018-05-25',2018,3,512103579,6,'prova5@unisa.it',3,4);
+INSERT INTO REQUEST VALUES (6,'B.6.56546','A1','2017-05-25','2018-05-25',2018,3,512103579,6,'prova6@unisa.it',3,3);
+
+INSERT INTO `user` VALUES ('prova2@unisa.it','Giuseppino','Bisoio','M','4bb47fd2a6c598d2a52ef7de3473fd3ea8401a9e',0, null);
+INSERT INTO `user` VALUES ('prova3@unisa.it','Giulia','Serio','F','4bb47fd2a6c598d2a52ef7de3473fd3ea8401a9c',0, null);
+INSERT INTO `user` VALUES ('prova@unisa.it','Lollo','Mat','M','4bb47fd2a6c598d2a52ef7de3473fd3ea8401a9d',0,null);
+INSERT INTO `user` VALUES ('prova4@unisa.it','Giuseppino','Bisoio','M','4bb47fd2a6c598d2a52ef7de3473fd3ea8401a9f',0,null);
+INSERT INTO `user` VALUES ('prova5@unisa.it','Giulia','Serio','F','4bb47fd2a6c598d2a52ef7de3473fd3ea8401a9g',0,null);
+INSERT INTO `user` VALUES ('prova6@unisa.it','Lollo','Mat','M','4bb47fd2a6c598d2a52ef7de3473fd3ea8401a9h',0,null);
+INSERT INTO `user` VALUES ('fferrucci@unisa.it','Filomena','Ferrucci','F','4bb47fd2a6c598d2a52ef7de3473fd3ea8401a9b',2,null),('segreteria@unisa.it','Segreteria','Studenti','M','4bb47fd2a6c598d2a52ef7de3473fd3ea8401a9b',1,null);
+
+/*USER RC*/
+INSERT INTO `user` VALUES ('provaRC1@unisa.it','PROVA1','RC1','M','12345678',0,'01-1-10');
+INSERT INTO `user` VALUES ('provaRC2@unisa.it','PROVA2','RC2','M','12345678',0,'2020-10-20');
+/*UC*/
+INSERT INTO UC VALUES('EMAILUC@gmail.it','12356789','3331231233','3331231233');
+/*REPORT*/
+INSERT INTO REPORT VALUES(1 ,'La sua carriera ok okok');
+INSERT INTO REPORT VALUES(2 ,'La sua carriera NON è ok okok');
+INSERT INTO REPORT VALUES(3 ,'La sua carriera NON è ok ok');
+/*UNIVERSITY*/
+INSERT INTO UNIVERSITY VALUES('UNINA');
+INSERT INTO UNIVERSITY VALUES('UNIBO');
+/*REQUEST*/
+INSERT INTO REQUEST_RC VALUES(1, '2019-3-11', 1,'UNINA','provaRC1@unisa.it',1,'EMAILUC@gmail.it');
+INSERT INTO REQUEST_RC VALUES(2, '2020-1-30', 2,'UNIBO','provaRC2@unisa.it',2,'EMAILUC@gmail.it');
+INSERT INTO REQUEST_RC VALUES(3, '2020-4-10', 3,'UNIBO','prova2@unisa.it',1,'EMAILUC@gmail.it');
+
+/*VALIDATE EXAM*/
+INSERT INTO VALIDATE_EXAM VALUES(1,'PROGRAMMAZIONE 1', 9, 'è stato validato bene',1);
+INSERT INTO VALIDATE_EXAM VALUES(2,'ANALISI 1', 0, 'non sono stati validati cfu',2);
+INSERT INTO VALIDATE_EXAM VALUES(3,'ANALISI 1', 0, 'non sono stati validati cfu',3);
+/*PDF*/
+INSERT INTO FILE_PDF VALUES(1, 'FILEPDFCARR1',1);
+INSERT INTO FILE_PDF VALUES(2, 'FILEPDFDOC1',1); 
+INSERT INTO FILE_PDF VALUES(3, 'FILEPDFCARR2',2);
+INSERT INTO FILE_PDF VALUES(4, 'FILEPDFDOC2',2);
+/*EXAM*/
+INSERT INTO EXAM VALUES(1, 'Programmazione 1', 12, 'www.sito.it/dati/esame/prog1');
+INSERT INTO EXAM VALUES(2, 'Analisi 1', 9, 'www.sito.it/dati/esame/anal1');
+insert into exam values (3,'IS',9,'www.sito.it/dati/esame/IS');
+/*CONTAINS*/ 
+insert into contains values (1,1);
+insert into contains values (1,3);
+insert into contains values (2,2);
+
+INSERT INTO `attached` VALUES (1,'certificato.pdf',1,'prova@unisa.it');
 
 INSERT INTO STATE 
 VALUES (1,'Parzialmente Completata');
