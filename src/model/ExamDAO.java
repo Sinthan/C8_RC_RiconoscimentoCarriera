979c8c3b1a195d2cfced0cb6f37e74cb9c73dca6
Gianluca Rossi <Qlett@icloud.com>
2019-12-17 12:15:29 +0100

    Implemented some DAO insert methods for the request submission, commented some snippets of code
    1. FilePDF:
    - Implemented Constructors.
    - Documented some methods.
    
    2. FilePDFDAO:
    - Implemented the insertFilePDF method.
    
    3. ValidatedExam:
    - Implemented Constructors.
    - Documented some methods.
    
    4. ExamDAO:
    - Implemented the insertExam method.

--- src/model/ExamDAO.java
+++ src/model/ExamDAO.java
@@ -2,6 +2,8 @@ package model;
 import model.Exam;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
 
@@ -21,12 +23,49 @@ public class ExamDAO implements ExamDAOInterface{
 	Connection conn = (Connection) new DbConnection().getInstance().getConn();
 	
 	/**
-	 * insert new exam 
-	 * @return -1 if insert failed, 0 if ok 
+	 * Saves the exam into the database.
+	 * 
+	 * @param exam	the <tt>Exam</tt> object that will be saved.
+	 * @return		<ul><li>a positive count of the number of rows affected (from INSERT, UPDATE, or DELETE)
+	 *				<li>0 if no rows were affected
+	 *				<li>-1 if the statement succeeded, but there is no update count information available</ul>
+	 *				<li>-2 if the attributes of the passed argument aren't fully specified
+	 * @throws 		SQLException
+	 * @author 		Gianluca Rossi
 	 */
-	public int insertExam(Exam exam) {
-		int flag = 0;
-		return flag;
+	public int insertExam(Exam exam) throws SQLException {
+		if (exam.getName().equals("") && exam.getCFU() == -1 && exam.getProgramLink().equals("")) // Checks if attributes are set
+			return -2;
+		Connection connection = null;
+		PreparedStatement preparedStatement = null;		
+		int result = 0;
+
+		/* Adds the 2 parametric values in the EXAM table.
+		 * The exam ID is automatically generated from the
+		 * database, as it is defined as an auto increment value,
+		 * so it's not required to create a new one here.
+		 */
+		String insertSQL = "INSERT INTO EXAM " +
+				" (NAME, CFU, LINK_PROGRAM) " +
+				" VALUES (?, ?, ?)";
+		try {
+			connection = DbConnection.getInstance().getConn();
+			preparedStatement = connection.prepareStatement(insertSQL);			
+			// Setting parameters
+			preparedStatement.setString(1, exam.getName());
+			preparedStatement.setInt(2, exam.getCFU());
+			preparedStatement.setString(3, exam.getProgramLink());
+			// Logging the operation
+			System.out.println("insertExam: "+ exam.toString());
+
+			result = preparedStatement.executeUpdate();	
+			connection.commit();
+		} finally {
+			// Statement release
+			if(preparedStatement != null)
+				preparedStatement.close();
+		}
+		return result;
 	}
 	
 	/**
