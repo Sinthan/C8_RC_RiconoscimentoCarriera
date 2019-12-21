b3999161f6104b5585c39e183273138d416278fd
andreacella <56684150+andreacella@users.noreply.github.com>
2019-12-21 21:02:43 +0100

    Update FilePDFDAOTest.java
    Test sui DAO

--- src/test/FilePDFDAOTest.java
+++ src/test/FilePDFDAOTest.java
@@ -19,11 +19,42 @@ import model.UCDAO;
 class FilePDFDAOTest {
 
 	FilePDFDAO filePDFDAO;
+	FilePDF filePDF;
 	
 	@BeforeEach
-	public void setUp() {
+	public void setUp() throws Exception {
 		filePDFDAO = new FilePDFDAO();
+		filePDF = new FilePDF();
 	}
+	
+	/*@Test
+	void testInsertFilePDFOK() throws SQLException {
+		filePDF.setPDFLink("FILEPDFCARR1");
+		filePDF.setRequestRCID(1);
+		
+		int i = filePDFDAO.insertFilePDF(filePDF);
+		assertEquals(-1, i);
+	}
+*/
+	
+	@Test
+	void testInsertFilePDFFailPDFLink() {
+		filePDF.setPDFLink("");
+		filePDF.setRequestRCID(1);
+		
+		int i = filePDFDAO.insertFilePDF(filePDF);
+		assertEquals(-2, i);
+	}
+	
+	@Test
+	void testInsertFilePDFFailRequestRCID() {
+		filePDF.setPDFLink("FILEPDFCARR1");
+		filePDF.setRequestRCID(-1);
+		
+		int i = filePDFDAO.insertFilePDF(filePDF);
+		assertEquals(-2, i);
+	}
+	
 	
 	
 }	
