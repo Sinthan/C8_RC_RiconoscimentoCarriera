a96e057ab8394a558f313e26f32dd67a84e9a065
Gianluca Rossi <Qlett@icloud.com>
2019-12-21 09:37:01 +0100

    Update StudentManagement.java
    Added some comments, removed imports, reorganized code

--- src/controller/StudentManagement.java
+++ src/controller/StudentManagement.java
@@ -8,19 +8,8 @@ import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 
 import java.io.File;
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.sql.Date;
-import java.util.ArrayList;
 
-import javax.servlet.RequestDispatcher;
-import javax.servlet.ServletException;
-import javax.servlet.ServletRequest;
 import javax.servlet.annotation.MultipartConfig;
-import javax.servlet.annotation.WebInitParam;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -33,28 +22,17 @@ import model.RequestRC;
 
 import javax.servlet.http.Part;
 
-import org.apache.catalina.User;
-import org.json.simple.JSONObject;
-
-import com.itextpdf.text.log.SysoCounter;
-
-import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 import model.ContainsRelation;
 import model.ContainsRelationDAO;
 import model.Exam;
 import model.ExamDAO;
 import model.FilePDF;
 import model.FilePDFDAO;
-import model.RequestRC;
 import model.SortByName;
-import model.Student;
 import model.University;
 import model.UniversityDAO;
 import java.util.Collections;
-import java.util.Comparator;
+import java.util.List;
 
 
 /**
@@ -73,48 +51,48 @@ public class StudentManagement extends HttpServlet {
 	static String SAVE_DIR = "./DocumentsRC";
 	static String SAVE_DIR2 = "C:\\Users\\" + System.getProperty("user.name");
 	String relativePathPDF = "/DocumentsRequestRC";
-	
-	
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public StudentManagement() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
+
+
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public StudentManagement() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		
-		int flag = (int) request.getSession().getAttribute("flag");
-		
-        if(flag == 0) {
-            HttpSession sessione = request.getSession(true);
 
-            Student s = (Student) sessione.getAttribute("user");
-            String email = s.getEmail();
-            
-            try {
-                RequestRCDAO rDAO =  new RequestRCDAO();
-                RequestRC rRC = rDAO.doRetrieveRequestRCByStudentID(email);
-                SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-                String date = sdf.format(rRC.getSubmissionDate());
-                  
-                    request.setAttribute("rRCDate", date);
-                    request.setAttribute("rRCState", rRC.getState().toString());
-                    
-                    RequestDispatcher requestDispatcher = request.getRequestDispatcher("/WEB-INF/GUIStudentRC/viewRCRequestStatus.jsp");
-                    requestDispatcher.forward(request, response);
-                    return;
-                    
-            }catch (Exception e) {
-                e.getMessage();
-            }
-        } else if( flag==1 ){
+		int flag = (int) request.getSession().getAttribute("flag");
+
+		if(flag == 0) {
+			HttpSession sessione = request.getSession(true);
+
+			Student s = (Student) sessione.getAttribute("user");
+			String email = s.getEmail();
+
+			try {
+				RequestRCDAO rDAO =  new RequestRCDAO();
+				RequestRC rRC = rDAO.doRetrieveRequestRCByStudentID(email);
+				SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+				String date = sdf.format(rRC.getSubmissionDate());
+
+				request.setAttribute("rRCDate", date);
+				request.setAttribute("rRCState", rRC.getState().toString());
+
+				RequestDispatcher requestDispatcher = request.getRequestDispatcher("/WEB-INF/GUIStudentRC/viewRCRequestStatus.jsp");
+				requestDispatcher.forward(request, response);
+				return;
+
+			}catch (Exception e) {
+				e.getMessage();
+			}
+		} else if( flag==1 ){
 			//accede al primo form
 			List<University> universities = uniDAO.doRetrieveAllUniversity();
 			Collections.sort(universities , new SortByName());
@@ -144,17 +122,17 @@ public class StudentManagement extends HttpServlet {
 				dis.forward(request, response);
 			}else {
 				Student s = (Student) request.getSession().getAttribute("user");
-				
+
 				File file1 = new File(SAVE_DIR2 + "");
 				if( !file1.mkdir() ) {
 					file1.mkdir();	
 				}
-				
+
 				File file2 = new File(SAVE_DIR2 + "\\" + s.getEmail());
 				if( !file2.mkdir() ) {
 					file2.mkdir();	
 				}
-				
+
 				try {
 					filePart1.write(SAVE_DIR2 + "\\" + s.getEmail() + "\\" + "ID.pdf");
 				}catch(Exception msg) {
@@ -165,50 +143,49 @@ public class StudentManagement extends HttpServlet {
 				}catch(Exception msg) {
 					System.out.println(msg.getLocalizedMessage());
 				}
-			
+
 				// Initialize FilePDF and RequestRC Object
 				FilePDF filePDF1 = new FilePDF();
 				FilePDF filePDF2 = new FilePDF();
 				RequestRC newRequestRC = new RequestRC();
-				
+
 				// Populate Request
 				newRequestRC.setUniversityID(UniSel);
 				newRequestRC.setStudentID(s.getEmail());
-				
+
 				// Populate FilePFD
 				filePDF1.setPDFLink(SAVE_DIR2 + "\\" + s.getEmail() + "\\" + "ID.pdf");
 				filePDF2.setPDFLink(SAVE_DIR2 + "\\" + s.getEmail() + "\\" + "ID.pdf");
-				
+
 				// Set FilePDF and RequestRC in the session
 				request.getSession().setAttribute("newRequestRC", newRequestRC);
 				request.getSession().setAttribute("filePDF1", filePDF1);
 				request.getSession().setAttribute("filePDF2", filePDF2);
-				
+
 				dis = request.getServletContext().getRequestDispatcher("/WEB-INF/GUIStudentRC/createRCRequest2.jsp");
 				dis.forward(request, response);
 			}
-			
+
 		} else if (flag == 3) { // Exams list insertion page (createRCRequest2.jsp)
 			// Adding the request to the database
 			RequestRC newReq = (RequestRC) request.getSession().getAttribute("newRequestRC");
 			RequestRCDAO reqDAO =  new RequestRCDAO();
-            int insertRequestRCResult = reqDAO.insertRequestRC(newReq);
+			int insertRequestRCResult = reqDAO.insertRequestRC(newReq);
 			// Getting RequestRC ID
 			String studentMail = newReq.getStudentID();
 			RequestRC dbRCRequest = reqDAO.doRetrieveRequestRCByStudentID(studentMail);
 			int reqRCID = dbRCRequest.getRequestRCID();
-            // Adding the exams to the database
-			System.out.println("rowCount" + request.getParameter("rowCount"));
-			int rowCount = Integer.parseInt(request.getParameter("rowCount"));
-			Exam exam = new Exam();
-			ExamDAO examDAO =  new ExamDAO();
-			int lastExamID = examDAO.doRetrieveMaxExamID();
+			// Initializing ContainsRelation between a RequestRC and its Exams 
 			ContainsRelationDAO containsRelDAO = new ContainsRelationDAO();
 			ContainsRelation containsRel = new ContainsRelation();
 			containsRel.setRequestRCID(reqRCID);
+			int examsInserted = Integer.parseInt(request.getParameter("rowCount"));
+			// Adding the exams to the database
+			Exam exam = new Exam();
+			ExamDAO examDAO =  new ExamDAO();
+			int lastExamID = examDAO.doRetrieveMaxExamID();
 			int currentExamID = lastExamID + 1;
-			System.out.println("currentExamID" + currentExamID);
-			for (int i = 1; i <= rowCount; i++) {
+			for (int i = 1; i <= examsInserted; i++) {
 				// Gets the exam parameters from the form
 				String n = "examName" + i;
 				System.out.println("n " + n);
@@ -218,20 +195,20 @@ public class StudentManagement extends HttpServlet {
 				System.out.println("CFU " + CFU);
 				String link = (String) request.getParameter("programLink" + i);
 				System.out.println("programLink " + link);
-				// Sets the exam attributes and adds it to the database
+				// Sets the exam attributes
 				exam.setName(name);
 				exam.setCFU(CFU);
 				exam.setProgramLink(link);
-				// Adding the contains relations to the database.
-	            int insertExamResult = examDAO.insertExam(exam);
-	            if (insertExamResult >= 0) { // Exam already present in the database
-	            	System.out.println("exam already present, insertExam result: " + insertExamResult);
-	            	containsRel.setExamID(insertExamResult);
-	            } else { // Exam was not present in the database
-	            	System.out.println("exam not present, insertExam result: " + insertExamResult);
-	            	containsRel.setExamID(currentExamID);
-	            	currentExamID++;
-	            }
+				// Adding the contains relations and the exam to the database.
+				int insertExamResult = examDAO.insertExam(exam);
+				if (insertExamResult >= 0) { // Exam record already present in the database
+					System.out.println("exam\"" + exam.getName() + "\"already present");
+					containsRel.setExamID(insertExamResult); // Sets the examID of the exam already stored
+				} else { // Exam isn't present in the database
+					System.out.println("exam\"" + exam.getName() + "\"not present");
+					containsRel.setExamID(currentExamID); // Sets the examID of the exam just added
+					currentExamID++;
+				}
 				containsRelDAO.insertContainsRelation(containsRel);
 			}
 			// Adding the PDF files to the database.
@@ -242,9 +219,9 @@ public class StudentManagement extends HttpServlet {
 			file2.setRequestRCID(reqRCID);
 			int insertFile1Result = pdfDAO.insertFilePDF(file1);
 			int insertFile2Result = pdfDAO.insertFilePDF(file2);
-			// Redirecting to the "view request status" page
+			// Redirecting to the "view request status" page and setting the attributes it will need
 			request.setAttribute("rRCDate", dbRCRequest.getSubmissionDate());
-            request.setAttribute("rRCState", dbRCRequest.getState());
+			request.setAttribute("rRCState", dbRCRequest.getState());
 			RequestDispatcher dis = request.getServletContext().getRequestDispatcher("/WEB-INF/GUIStudentRC/viewRCRequestStatus.jsp");
 			dis.forward(request, response);
 		}
