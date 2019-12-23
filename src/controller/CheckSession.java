886525ff38bcc08bd5a8771bd15d22e1e8468cf1
GerardoDam <57265408+GerardoDam@users.noreply.github.com>
2019-12-23 20:05:36 +0100

    Fix redirect for UC added screen for UC
    Redirect Fixed: when the user login the header test CheckSession class for the redirect, added condition for the UC.
    
    Created and added the view screen of a single RequestRc

--- src/controller/CheckSession.java
+++ src/controller/CheckSession.java
@@ -5,6 +5,7 @@ import java.io.Serializable;
 import javax.servlet.http.HttpSession;
 
 import interfacce.UserInterface;
+import model.UC;
 
 
 
@@ -99,23 +100,32 @@ public class CheckSession implements Serializable {
    */
   public boolean isAllowed() {
     this.setAllowed(false);
-    UserInterface u = (UserInterface) this.session.getAttribute("user");
-    if (u != null) {
-      int userType = u.getUserType();
+    Object uc = this.getSession().getAttribute("user");
+    if(uc instanceof UC && this.pageFolder.equals("GUIUC")){
+    	
+    	this.setAllowed(true); // Profilo UC
+    }else {
+    
+    	UserInterface u = (UserInterface) this.session.getAttribute("user");
+    	if (u != null) {
+    		int userType = u.getUserType();
 
-      if (userType == 0 && this.pageFolder.equals("_areaStudent")) {
-        this.setAllowed(true); // Profilo Student
+    		if (userType == 0 && this.pageFolder.equals("_areaStudent")) {
+    			this.setAllowed(true); // Profilo Student
 
-      } else if (userType == 1 && this.pageFolder.equals("_areaSecretary")) {
-        this.setAllowed(true); // Profilo Secretary
+    		} else if (userType == 1 && this.pageFolder.equals("_areaSecretary")) {
+    			this.setAllowed(true); // Profilo Secretary
 
-      } else if (userType == 2 && this.pageFolder.equals("_areaAdmin")) {
-        this.setAllowed(true); // Profilo Admin
-      }
+    		} else if (userType == 2 && this.pageFolder.equals("_areaAdmin")) {
+    			this.setAllowed(true); // Profilo Admin
+        
+    		} else if (this.pageFolder.equals("GUIUC")) {
+    			this.setAllowed(true); // Profilo UC	
+    		}
+    	}
     }
     return allowed;
   }
-
   /**
    * Set the variable allowed.
    * 
--- src/controller/UCManagement.java
+++ src/controller/UCManagement.java
@@ -1,23 +1,25 @@
 package controller;
 
 import java.io.IOException;
+
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
 /**
- * Servlet implementation class UCRequestRedirector
+ * Servlet implementation class UCManagement
  */
-@WebServlet("/UCRequestRedirector")
-public class UCRequestRedirector extends HttpServlet {
+@WebServlet("/UCManagement")
+public class UCManagement extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public UCRequestRedirector() {
+    public UCManagement() {
         super();
         // TODO Auto-generated constructor stub
     }
@@ -28,9 +30,8 @@ public class UCRequestRedirector extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		
-		RequestDispatcher requestDispatcher = request.getRequestDispatcher("/WEB-INF/GUIUC/homeRCUC.jsp");		
+		RequestDispatcher requestDispatcher = request.getRequestDispatcher("/WEB-INF/GUIUC/viewRCRequestUC.jsp");		
 		requestDispatcher.forward(request, response);
-
 	}
 
 	/**
