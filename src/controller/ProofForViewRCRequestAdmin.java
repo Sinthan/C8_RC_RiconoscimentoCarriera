646489f6bc2bdab8d13d093ca4dc2a83a3bd0575
Sinthan <a.cassese9@studenti.unisa.it>
2020-01-07 14:16:19 +0100

    Removed Checkstyle configuration
    Removed Checkstyle configuration on startup for better UX
    
    Indentation LoginManagement ProofRequet

--- src/controller/ProofForViewRCRequestAdmin.java
+++ src/controller/ProofForViewRCRequestAdmin.java
@@ -16,41 +16,41 @@ import model.Admin;
  */
 @WebServlet("/ProofForViewRCRequestAdmin")
 public class ProofForViewRCRequestAdmin extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-       
-	/**
-     * @see HttpServlet#HttpServlet()
-     */
-	public ProofForViewRCRequestAdmin() {
-    		super();
-    		// TODO Auto-generated constructor stub
-	}
+  private static final long serialVersionUID = 1L;
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) 
-			throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		Admin admin = new Admin();
-		admin.setEmail("fferrucci@unisa.it");
-		admin.setName("Filomena");
-		admin.setSex('M');
-		admin.setSurname("Ferrucci");
-		request.getSession().setAttribute("user", admin);
-		request.getSession().setAttribute("requestRCID", 12);
-		request.setAttribute("flag", 2);
-		RequestDispatcher requestDispatcher = request.getRequestDispatcher("/AdminManagement");
-		requestDispatcher.forward(request, response);
-	}
+  /**
+   * @see HttpServlet#HttpServlet()
+   */
+  public ProofForViewRCRequestAdmin() {
+    super();
+    // TODO Auto-generated constructor stub
+  }
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) 
-			throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
-	}
+  /**
+   * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+   */
+  protected void doGet(HttpServletRequest request, HttpServletResponse response)
+      throws ServletException, IOException {
+    // TODO Auto-generated method stub
+    Admin admin = new Admin();
+    admin.setEmail("fferrucci@unisa.it");
+    admin.setName("Filomena");
+    admin.setSex('M');
+    admin.setSurname("Ferrucci");
+    request.getSession().setAttribute("user", admin);
+    request.getSession().setAttribute("requestRCID", 12);
+    request.setAttribute("flag", 2);
+    RequestDispatcher requestDispatcher = request.getRequestDispatcher("/AdminManagement");
+    requestDispatcher.forward(request, response);
+  }
+
+  /**
+   * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+   */
+  protected void doPost(HttpServletRequest request, HttpServletResponse response)
+      throws ServletException, IOException {
+    // TODO Auto-generated method stub
+    doGet(request, response);
+  }
 
 }
